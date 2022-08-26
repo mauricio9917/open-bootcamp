7bfcd6876700c38dfaafc3f63f095733a1db1d71
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-08-26 08:23:14 -0500

    Add files via upload

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,10 @@
+
+// Este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto")
+
+/* Esta es la parte intermedia del proyecto
+sgkvjzbksjjirv
+ksdjbck <sefb sckvbos<jec */
+
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adiós")
