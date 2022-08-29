d3bc5a0921a4de80bdfd9fce5f15cee62d0ee2e9
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-08-29 10:05:57 -0500

    Add files via upload

---
+++ Curso-Javascript/ejercicio-2.js
@@ -0,0 +1,17 @@
+const nombre = "Yojan Mauricio"
+
+const edad = 23
+
+const esProgramador = true;
+
+const fechaNacimiento = new Date(1999, 04, 25)
+
+const libro_favorito = {
+    titulo: "El varón domado",
+    autor: "Ester Vilar",
+    fecha: new Date(1973, 08, 24),
+    url: "http://imagourbis.unq.edu.ar/uq_jaga/img/pdf/VILAR%20ESTHER%20-%20El%20Varon%20Domado.pdf"
+}
+
+const listaDatos = [nombre, edad, esProgramador, fechaNacimiento, libro_favorito]
+console.log(listaDatos)
