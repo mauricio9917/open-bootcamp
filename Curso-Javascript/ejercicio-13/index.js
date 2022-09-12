bb78c93db4b455f77de237341b0f1cebe5221df5
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-09-12 17:42:33 -0500

    Add files via upload

---
+++ Curso-Javascript/ejercicio-13/index.js
@@ -0,0 +1,38 @@
+// Sesion 4 - Strings 
+
+let str_dbl = "es un texto con comillas dobles"
+let str_sng = "es un texto con comillas simples"
+
+console.log(str_dbl)
+console.log(str_sng)
+
+let str_comillas = "El otro dia me dijo literalmente \"ve a sacar la basura\""
+let str_comillas_simples = "El otro dia me dijo literalmente \"ve a sacar la basura\""
+let str_comillas_dobles = "El otro dia me dijo literalmente 've a sacar la basura'"
+let str_comillas_simples_2 = "El otro dia me dijo literalmente 've a sacar la basura'"
+
+console.log(str_comillas)
+console.log(str_comillas_simples)
+console.log(str_comillas_dobles)
+console.log(str_comillas_simples_2)
+
+// Comillas invertidas (backticks)
+let str_backticks = "Este es un texto con backticks"
+
+console.log(str_backticks)
+
+let nombre = "Mauricio"
+let saludo = `Hola, ${nombre} bienvenido.`
+
+console.log(saludo)
+
+// Plantillas HTML
+let plantilla = `
+<html>
+    <h1>Titulo</h1>
+<html>
+`
+console.log(plantilla)
+
+//Introducción de variables en HTML
+let libros = ["suvinc as", "dkzjvbkjsnf", "ksgcbkw"]
