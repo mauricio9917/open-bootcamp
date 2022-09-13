da1dbc022ef404c7d7137a500361bdc2995efc3e
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-09-13 15:35:29 -0500

    Add files via upload

---
+++ Curso-Javascript/ejercicio-15/index.js
@@ -0,0 +1,11 @@
+let miNombre = "Mauricio"
+let miApellido = "Ardila"
+
+const persona = {
+    miNombre,
+    miApellido
+}
+
+// sessionStorage.getItem(persona)
+// localStorage.getItem(persona)
+// document.cookie = "persona=persona; expires="+ new Date(2022,8,13,15,33,30,30).toUTCString()
