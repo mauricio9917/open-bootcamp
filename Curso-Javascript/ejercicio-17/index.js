d5d13ccadb9005005934ddaefbd84be99970a08e
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-09-15 10:12:51 -0500

    Update index.js

--- Curso-Javascript/ejercicio-17/index.js
+++ Curso-Javascript/ejercicio-17/index.js
@@ -1,17 +1,46 @@
+let markers, map
 // Initialize and add the map
 function initMap() {
-    const uluru = { lat: -25.344, lng: 131.031 }
-    const tayrona = {lat: 11.3270989,lng: -74.0843038}
-    const redSquare = {lat: 55.7534063, lng: 37.6192392}
+    const posicion = {
+        lat: -25.344, 
+        lng: 131.031
+    }
+
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: {uluru, tayrona, redSquare}
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: (uluru,tayrona,redSquare),
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+        zoom: 4,
+        center: posicion,
+    })
+
+    markers.push(
+        new google.maps.Marker({
+            posicion: {
+                lat: 11.3270989,
+                lng: -74.0843038
+            },
+            map,
+            title: "Tayrona"
+        })
+    )
+
+    markers.push(
+        new google.maps.Marker({
+            posicion: {
+                lat: 55.7534063,
+                lng: 37.6192392
+            },
+            map,
+            title: "Plaza Roja"
+        })
+    )
+
+    markers.push(
+        new google.maps.Marker({
+            posicion: {
+                lat: -8.727807,
+                lng: 115.5444231
+            },
+            map,
+            title: "Bali"
+        })
+    )
+}
