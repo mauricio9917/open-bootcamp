122d297b60fd4f2707b099b60f393e61732aceff
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-09-29 09:51:01 -0500

    Update contact.jsx

--- React-JS/ejercicio-7/src/components/contact.jsx
+++ React-JS/ejercicio-7/src/components/contact.jsx
@@ -1,73 +1,53 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
-import {Contact} from '../models/contact.class'
+import React, { useState } from 'react';
+import ContactComponent from './contactComponent';
+import ContactForm from './contactForm';
 
-// se importa la hoja de estilos de css
-// import '../../styles/contact.scss'
-
-
-const ContactComponent = ({contact, connected, remove}) => {
-
-    useEffect(() => {
-        console.log('Created Task')
-        return () => {
-            console.log(`Task: ${contact.name} is going to unmount`)
-        };
-    }, [contact]);
-
-    /**
-     * Function that returns a badge depending on the level of the contact
-     */
-
-    /**
-     * Function that returns icon depending on completation of the contact
-     */
-    function connectedIcon(){
-        if(contact.connected){
-            return (<i onClick={() => connected(contact)} className='bi-toggle-on contact-action' style={{color:'green'}}></i>)
-        } else {
-            return (<i onClick={() => connected(contact)} className='bi-toggle-off contact-action' style={{color:'gray'}}></i>)
-        }
-    }
-
-    return (
-        <tr className="fw-normal">
-            <th>
-                <span className="ms-2">{contact.name}</span>
-            </th>
-            <th>
-                <span className="ms-2">{contact.last_name}</span>
-            </th>
-            <td className="align-middle">
-                <span>{contact.email}</span>
-            </td>
-            <td className="align-middle">
-                {connectedIcon()}
-                <i className='bi-trash contact-action' style={{color:'tomato'}} onClick={() => remove(contact)}></i>
-            </td>
-        </tr>
-        // <div>
-        //     <h2 className='contact-name'>
-        //         Nombre: {contact.name}
-        //     </h2>
-        //     <h3>
-        //         Descripcion: {contact.description}
-        //     </h3>
-        //     <h4>
-        //         Level: {contact.level}
-        //     </h4>
-        //     <h5>
-        //         This class is: {contact.completed ? 'COMPLETED':'PENDING'}
-        //     </h5>
-        // </div>
-    );
-};
+const Contact = ( ) => {
+    const defaultContact = [
+        { nombre: 'Jose', email: 'Fernandez', conectado: true },
+        { nombre: 'Maria', email: 'Gómez', conectado: true },
+        ];
+    
+      const [nuevoContacto, setNuevoContacto] = useState(defaultContact);
+    
+      function changeState(contacto) {
+        const index = nuevoContacto.indexOf(contacto);   
+        const tempContact = [...nuevoContacto];
+    
+        tempContact[index].estado = !tempContact[index].estado;
+        setNuevoContacto(tempContact);
+      }
+    
+      function remove(contacto) {
+        const index = nuevoContacto.indexOf(contacto);
+        const tempContact = [...nuevoContacto];
+        tempContact.splice(index, 1);
+        setNuevoContacto(tempContact);
+      }
+    
+      function addContact(contacto) {
+        const tempContact = [...nuevoContacto];
+        tempContact.push(contacto);
+        setNuevoContacto(tempContact);
+      }
+    
+      return (
+        <div className="Contacto">
+          <h1>Contactos</h1>
+    
+          <div className="card-container">
+            {nuevoContacto.map((contacto, index) => {
+              return (
+                <ContactComponent key={index} contacto={contacto} changeState={changeState} remove={remove} />
+              );
+            })}
+          </div>
+    
+          <ContactForm onAddContact={addContact}></ContactForm>
+        </div>
+      );
+    };
+    
 
 
-ContactComponent.propTypes = {
-    contact: PropTypes.instanceOf(Contact).isRequired,
-    remove: PropTypes.func.isRequired
-};
-
-
-export default ContactComponent;
+export default Contact;
