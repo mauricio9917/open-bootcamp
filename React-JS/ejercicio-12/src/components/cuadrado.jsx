1ec1719f3adf2b1dcbb8c43786915c71353e7c46
mauricio9917 <90358774+mauricio9917@users.noreply.github.com>
2022-09-29 14:12:01 -0500

    Update cuadrado.jsx

--- React-JS/ejercicio-12/src/components/cuadrado.jsx
+++ React-JS/ejercicio-12/src/components/cuadrado.jsx
@@ -1,88 +1,141 @@
 import React, {useState} from 'react';
 
-const r = 100
-const g = 100
-const b = 100
-
-const figureOut = {
-    'weight': '255px',
-    'height': '255px',
-    'backgroundColor': (r,g,b)
-}
-
-const figureIn = {
-    'weight': '255px',
-    'height': '255px',
-    'backgroundColor': () => {
-        
-        const background = document.getElementById("background");
-
-        const getRandomNumber = (maxNum) => {
-        return Math.floor(Math.random() * maxNum);
-        };
-
-        const getRandomColor = () => {
-        const r = getRandomNumber(100);
-        const g = getRandomNumber(100);
-        const b = getRandomNumber(100);
-
-        return `rgb(${r}, ${g}, ${b})`;
-        };
-
-        const setBackgroundColor = () => {
-        const randomColor = getRandomColor();
-        background.style.backgroundColor = randomColor;
-        background.style.color = randomColor;
-        };
-
-        setBackgroundColor();
-
-        setInterval(() => {
-        setBackgroundColor();
-        }, 1000);
-    }
-}
-
-const InFigure = ({inAction, propStyle}) => {
-    return (
-        <div style={propStyle} onClick={inAction}></div>
-    )
-}
-
-const OutFigure = ({outAction, propStyle}) => {
-    return (
-        <div style={propStyle} onClick={outAction}></div>
-    )
-}
-
 const Cuadrado = () => {
+    const [color, setColor] = useState('#00000')
 
-    const [enter, setEnter] = useState(false)
-    const [color, setColor] = useState([r,g,b])
+    const [manageInterval, setManageInterval] = useState(0)
+    const [doubleClick, setDoubleClick] = useState(0)
 
-    const inAction = () => {
-        setEnter(true)
-        setColor(figureIn)
+    const getColor = () => Math.floor(Math.random() * 256)
+
+    const getHex = (red,green,blue) => {
+        return (
+            '#' + [red,green,blue].map((c) => {
+                const hex = c.toString(16)
+                return hex.lenght === 1 ? '0' + hex : hex
+            })
+            .join('')
+        )
     }
 
-    const outAction = () => {
-        setEnter(false)
-        setColor(color)
+    const generateHex = () => {
+        const rgb = {
+            r: getColor(),
+            g: getColor(),
+            b: getColor(),
+        }
+        return setColor(getHex(rgb.r, rgb.g, rgb.b))
     }
 
-    let optionalHover
+    const onChangeColor = () => {
+        return setManageInterval(setInterval(generateHex, 700))
+    }
 
-    if(enter){
-        optionalHover = <InFigure propStyle={figureIn} inAction={inAction}></InFigure>
-    } else {
-        optionalHover = <OutFigure propStyle={figureOut} outAction={outAction}></OutFigure>
+    const onStopChangeColor = () => {
+        return clearInterval(manageInterval)
+    }
+
+    const onClickChangeColor = () => {
+        return clearInterval(manageInterval)
     }
 
     return (
-        <div>
-            {optionalHover}
-        </div>
-    );
+        <div style={{ margin: 'auto' }}>
+        <div
+        id="square"
+            onMouseOver={onChangeColor}
+            onMouseLeave={onStopChangeColor}
+            onDoubleClick={onClickChangeColor}
+            style={{ width: '255px', height: '255px', backgroundColor: color, margin: 'auto' }}
+        ></div>{' '}
+        <p style={{ color: 'black' }}>Color: {color} </p>
+    </div>
+    )
 }
+
+// const r = 100
+// const g = 100
+// const b = 100
+
+// const figureOut = {
+//     'weight': '255px',
+//     'height': '255px',
+//     'backgroundColor': (r,g,b)
+// }
+
+// const figureIn = {
+//     'weight': '255px',
+//     'height': '255px',
+//     'backgroundColor': () => {
+        
+//         const background = document.getElementById("background");
+
+//         const getRandomNumber = (maxNum) => {
+//         return Math.floor(Math.random() * maxNum);
+//         };
+
+//         const getRandomColor = () => {
+//         const r = getRandomNumber(100);
+//         const g = getRandomNumber(100);
+//         const b = getRandomNumber(100);
+
+//         return `rgb(${r}, ${g}, ${b})`;
+//         };
+
+//         const setBackgroundColor = () => {
+//         const randomColor = getRandomColor();
+//         background.style.backgroundColor = randomColor;
+//         background.style.color = randomColor;
+//         };
+
+//         setBackgroundColor();
+
+//         setInterval(() => {
+//         setBackgroundColor();
+//         }, 1000);
+//     }
+// }
+
+// const InFigure = ({inAction, propStyle}) => {
+//     return (
+//         <div style={propStyle} onClick={inAction}></div>
+//     )
+// }
+
+// const OutFigure = ({outAction, propStyle}) => {
+//     return (
+//         <div style={propStyle} onClick={outAction}></div>
+//     )
+// }
+
+// const Cuadrado = () => {
+
+//     const [enter, setEnter] = useState(false)
+    
+
+//     const inAction = () => {
+//         setEnter(true)
+//         setColor(figureIn)
+//     }
+
+//     const outAction = () => {
+//         setEnter(false)
+//         setColor(color)
+//     }
+
+//     let optionalHover
+
+//     if(enter){
+//         optionalHover = <InFigure propStyle={figureIn} inAction={inAction}></InFigure>
+//     } else {
+//         optionalHover = <OutFigure propStyle={figureOut} outAction={outAction}></OutFigure>
+//     }
+
+//     return (
+//         <div>
+//             {optionalHover}
+//         </div>
+//     );
+// }
 
 export default Cuadrado;
